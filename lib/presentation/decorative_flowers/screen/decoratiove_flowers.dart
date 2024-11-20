53be7507d489d9952f2903e9469922986b61e5d7
mustafa-dev-nasr <mustafa.dev.nasr@gmail.com>
2024-11-20 18:09:47 +0200

    test

--- lib/presentation/decorative_flowers/screen/decoratiove_flowers.dart
+++ lib/presentation/decorative_flowers/screen/decoratiove_flowers.dart
@@ -6,7 +6,7 @@ import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_painter_rps.dart';
 import '../widgets/decorative_flowers_list.dart'; // Import the correct CustomPainter file
 
-class DecorativeFlowers extends StatelessWidget {
+class DecorativeFlowers extends StatelessWidget {//
   const DecorativeFlowers({super.key});
 
   @override
